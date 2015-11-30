0183147cde580ed4981e084ebbc2c1e23d95919d
玄恒 <yuedong.lyd@alibaba-inc.com>
2015-11-30 19:18:00 +0800

    完善例子

--- align-text-view-example/src/main/java/me/codeboy/android/aligntextview/example/AlignTextViewExample.java
+++ align-text-view-example/src/main/java/me/codeboy/android/aligntextview/example/AlignTextViewExample.java
@@ -7,6 +7,8 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
 
+import me.codeboy.android.aligntextview.CBAlignTextView;
+
 /**
  * AlignTextView例子
  *
@@ -17,27 +19,31 @@ public class AlignTextViewExample extends Activity {
     TextView textViewTv;
     TextView alignTv;
     TextView justifyTv;
-    TextView cbAlignTv;
+    CBAlignTextView cbAlignTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
- WindowManager.LayoutParams.FLAG_FULLSCREEN);
-                setContentView(R.layout.activity_align_text_view);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager
+                .LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_align_text_view);
 
         textViewTv = (TextView) findViewById(R.id.textview);
         justifyTv = (TextView) findViewById(R.id.justifyTextView);
         alignTv = (TextView) findViewById(R.id.alignTextView);
-        cbAlignTv = (TextView) findViewById(R.id.cbAlignTextView);
+        cbAlignTv = (CBAlignTextView) findViewById(R.id.cbAlignTextView);
 
-        String text = "这是一段中英文混合的文本，I am very happy today. aaaaaaaaaa,测试TextView文本对齐\n\nAlignTextView可以通过setAlign()方法设置每一段尾行的对齐方式,默认尾行居左对齐.CBAlignTextView可以像原生TextView一样操作，但是需要使用getRealText()获取文本,欢迎访问open.codeboy.me";
+        final String text = "这是一段中英文混合的文本，I am very happy today。 aaaaaaaaaa," +
+                "测试TextView文本对齐\n\nAlignTextView可以通过setAlign()方法设置每一段尾行的对齐方式, 默认尾行居左对齐. " +
+                "CBAlignTextView可以像原生TextView一样操作, 但是需要使用getRealText()获取文本, 欢迎访问open.codeboy.me";
         textViewTv.setText(text);
         justifyTv.setText(text);
         alignTv.setText(text);
+//        cbAlignTv.setPunctuationConvert(true);
         cbAlignTv.setText(text);
 
         alignTv.setMovementMethod(new ScrollingMovementMethod());
+
     }
 }
