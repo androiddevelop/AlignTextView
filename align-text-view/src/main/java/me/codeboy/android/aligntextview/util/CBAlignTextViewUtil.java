c380dd3468195fefb92f6d1a3c0d437a92376f4c
yuedong <androiddevelop@qq.com>
2017-10-16 08:20:43 +0800

    添加注释

--- align-text-view/src/main/java/me/codeboy/android/aligntextview/util/CBAlignTextViewUtil.java
+++ align-text-view/src/main/java/me/codeboy/android/aligntextview/util/CBAlignTextViewUtil.java
@@ -9,6 +9,7 @@ public class CBAlignTextViewUtil {
     /**
      * 将中文标点替换为英文标点
      *
+     * @param text 要替换的文本
      * @return 替换后的文本
      */
     public static String replacePunctuation(String text) {
